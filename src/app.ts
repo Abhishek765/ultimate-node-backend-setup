66c6e19a16044571788328f7a698af71521bfc5a
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-09-05 02:24:10 +0530

    feat: 404 handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import path from 'path';
 import router from './router/apiRouter';
 import globalErrorHandler from './middlewares/globalErrorHandler';
+import httpError from './utils/httpError';
+import responseMessages from './constants/responseMessages';
 
 const app: Application = express();
 
@@ -11,6 +13,17 @@ app.use(express.static(path.join(__dirname, '../', 'public')));
 
 // Routes
 app.use('/api/v1', router);
+
+// 404 error handler
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  try {
+    throw new Error(responseMessages.NOT_FOUND('route'));
+  } catch (error) {
+    httpError(next, error, req, 404);
+  }
+});
+
+// globalErrorHandler
 app.use(globalErrorHandler);
 
 export default app;
