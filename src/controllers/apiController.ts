b878b9763ab807124913b71e39211126d3621d0b
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-12-14 16:16:36 +0530

    feat: health endpoint

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,12 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
 
 import responseMessages from '../constants/responseMessages';
+import {
+  getApplicationHealth,
+  getSystemHealth
+} from '../utils/helperFunctions';
 import httpError from '../utils/httpError';
 import httpResponse from '../utils/httpResponse';
 
 export const self = (req: Request, res: Response, next: NextFunction) => {
   try {
     httpResponse(req, res, 200, responseMessages.SUCCESS);
+  } catch (err) {
+    httpError(next, err, req, 500);
+  }
+};
+
+export const health = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const healthData = {
+      application: getApplicationHealth(),
+      system: getSystemHealth(),
+      timeStamp: Date.now()
+    };
+    httpResponse(req, res, 200, responseMessages.SUCCESS, healthData);
   } catch (err) {
     httpError(next, err, req, 500);
   }
--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { self } from '../controllers/apiController';
+import { health, self } from '../controllers/apiController';
 
 const router = Router();
 
 router.route('/self').get(self);
+router.route('/health').get(health);
 
 // // Create a route for authenticated users
 // router.route('/authenticated').get((_req, _res) => {
