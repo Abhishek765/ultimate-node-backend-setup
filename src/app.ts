8d9e6e6d54b9f305f1cefbdd6e1b03a04fac6117
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-12-14 17:20:17 +0530

    feat: addition of helmet

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import helmet from 'helmet';
 import path from 'path';
 
 import globalErrorHandler from './middlewares/globalErrorHandler';
@@ -8,6 +9,7 @@ import router from './router/apiRouter';
 const app: Application = express();
 
 // middlewares
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
